43208437efb8cf307e73679969d46bd59416b881
Scarlett Eller <rq6715@gmail.com>
2022-01-27 13:30:33 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+    for (let i = 0; i < array.length; i++) {
+      const complement = target - array[i];
+        for (let j = i + 1; j < array.length; j++) {
+          if (array[j] === complement) return true;
+        }
+    }
+    return false;
+  }
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +14,6 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
